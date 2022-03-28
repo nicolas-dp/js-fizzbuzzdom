ed9aa768dc5e67b759de6ad7a9548818b0e1361d
Nicolas <wellcomenicolas@gmail.com>
2022-03-28 16:39:52 +0200

    Final Project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,44 @@
 
 //Selezione il div con classe numbers
-const elementNumbers = document.querySelector(".numbers")
+let elementNumbers = document.querySelector(".numbers")
 
 //Inizializzo il ciclo FOR per il conteggio numerico
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    const markupLi = `
+    let markupLi;
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i + "Multiplo di 3 e 5");
+        markupLi = `
+            <div class="number fizz_buzz d-flex justify-content-center align-items-center">
+                 <div>fizzbuzz</div>
+            </div>`
+        elementNumbers.innerHTML += markupLi;
+
+    } else if (i % 5 == 0) {
+        console.log(i + "Multiplo 5");
+        markupLi = `
+            <div class="number buzz d-flex justify-content-center align-items-center">
+                 <div>buzz</div>
+            </div>`
+        elementNumbers.innerHTML += markupLi;
+
+    } else if (i % 3 == 0) {
+        console.log(i + "Multiplo 3");
+        markupLi = `
+            <div class="number fizz d-flex justify-content-center align-items-center">
+                 <div>fizz</div>
+            </div>`
+        elementNumbers.innerHTML += markupLi;
+    } else {
+        console.log(i);
+        markupLi = `
     <div class="number d-flex justify-content-center align-items-center">
         <div>${i}</div>
     </div>`
 
-    elementNumbers.innerHTML += markupLi;
-
-    if (i % 3 == 0) {
-        console.log(i);
-        const markupLi = `
-        <div class="number fizz d-flex justify-content-center align-items-center">
-            <div>Fizz</div>
-        </div>`
-
         elementNumbers.innerHTML += markupLi;
     }
+
+
 
 
 }
